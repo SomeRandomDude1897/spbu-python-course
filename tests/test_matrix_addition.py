ded850fdc8afe9dfabb99b8e34666feea67e613b
SomeRandomDude1897 <sokhanivan@yandex.ru>
2024-09-26 17:03:57 +0300

    fixed codestyle in test_matrix_addition.py

--- tests/test_matrix_addition.py
+++ tests/test_matrix_addition.py
@@ -1,16 +1,26 @@
 import pytest
 from project.matrix import Matrix
 
+
 def test_matrix_addition():
     matrix1 = Matrix([[1, 2], [3, 4]])
     matrix2 = Matrix([[5, 6], [7, 8]])
     result = matrix1 + matrix2
-    assert result.content == [[6, 8], [10, 12]], "Результат сложения должен быть [[6, 8], [10, 12]]"
+    assert result.content == [
+        [6, 8],
+        [10, 12],
+    ], "Результат сложения должен быть [[6, 8], [10, 12]]"
 
     matrix3 = Matrix([[0, 0], [0, 0]])
     result = matrix1 + matrix3
-    assert result.content == [[1, 2], [3, 4]], "Результат сложения с нулевой матрицей должен быть равен оригинальной матрице"
+    assert result.content == [
+        [1, 2],
+        [3, 4],
+    ], "Результат сложения с нулевой матрицей должен быть равен оригинальной матрице"
 
     matrix4 = Matrix([[1, 2]])
-    with pytest.raises(Exception, match="In order to find the sum of two matrices they must be the same size"):
+    with pytest.raises(
+        Exception,
+        match="In order to find the sum of two matrices they must be the same size",
+    ):
         matrix1 + matrix4
